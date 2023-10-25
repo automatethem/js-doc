5bcf0c0ca92f71881e5490d161180af0653b7192
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-10-26 03:33:24 +0900

    Add files via upload

---
+++ array.js
@@ -0,0 +1,8 @@
+Array.from
+
+https://merrily-code.tistory.com/158
+https://velog.io/@teihong93/Array.from%EC%9D%84-%ED%86%B5%ED%95%9C-%EB%B0%B0%EC%97%B4%EC%9D%98-%EC%B4%88%EA%B8%B0%ED%99%94
+
+  const [nums, setNums] = useState(Array.from({ length: numBalls }));
+
+Array.from(6)
