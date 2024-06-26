d6f31ae271dfdec8b4e5245d1cbb16128ee37f67
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2024-06-26 17:08:56 +0900

    Update regex.js

--- regex.js
+++ regex.js
@@ -1,3 +1,26 @@
+
+const s = '[현대] 더 뉴아반떼MD 1.6 GDi 프리미엄';
+console.log(s); // [현대] 더 뉴아반떼MD 1.6 GDi 프리미엄
+
+// matchAll을 사용하여 정규표현식으로 매칭된 결과를 얻습니다.
+const matchAllResults = s.matchAll(/\[(.+?)\]/g);
+console.log(matchAllResults);
+
+for (const match of matchAllResults) {
+    console.log(match[0]); // 대괄호 포함
+    console.log(match[1]);
+}
+
+/*
+[현대] 더 뉴아반떼MD 1.6 GDi 프리미엄
+Object [RegExp String Iterator] {}
+[현대]
+현대
+*/
+
+
+---
+
 const s = '[현대] 더 뉴아반떼MD 1.6 GDi 프리미엄';
 console.log(s); //[현대] 더 뉴아반떼MD 1.6 GDi 프리미엄
 
@@ -26,23 +49,3 @@ makerNames.forEach(makerName => {
 */
 
 ---
-
-const s = '[현대] 더 뉴아반떼MD 1.6 GDi 프리미엄';
-console.log(s); // [현대] 더 뉴아반떼MD 1.6 GDi 프리미엄
-
-// matchAll을 사용하여 정규표현식으로 매칭된 결과를 얻습니다.
-const regex = /\[(.+?)\]/g;
-const matchAllResults = s.matchAll(regex);
-console.log(matchAllResults);
-
-for (const match of matchAllResults) {
-    console.log(match[0]); // 대괄호 포함
-    console.log(match[1]);
-}
-
-/*
-[현대] 더 뉴아반떼MD 1.6 GDi 프리미엄
-Object [RegExp String Iterator] {}
-[현대]
-현대
-*/
