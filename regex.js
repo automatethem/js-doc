db8c9481bf3a0c49f61d5ddb676914b30e2ff443
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2024-06-26 17:00:28 +0900

    Update regex.js

--- regex.js
+++ regex.js
@@ -24,3 +24,23 @@ makerNames.forEach(makerName => {
 현대
 현대
 */
+
+---
+
+const s = '[현대] 더 뉴아반떼MD 1.6 GDi 프리미엄';
+console.log(s); // [현대] 더 뉴아반떼MD 1.6 GDi 프리미엄
+
+// matchAll을 사용하여 정규표현식으로 매칭된 결과를 얻습니다.
+const regex = /\[(.+?)\]/g;
+const matchAllResults = s.matchAll(regex);
+
+for (const match of matchAllResults) {
+    console.log(match[0]); // 대괄호 포함
+    console.log(match[1]);
+}
+
+/*
+[현대] 더 뉴아반떼MD 1.6 GDi 프리미엄
+[현대]
+현대
+*/
