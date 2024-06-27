dfc4cb54ed634fe996935e19ef0c2a5d8c05987a
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2024-06-27 10:16:33 +0900

    Update regex.js

--- regex.js
+++ regex.js
@@ -3,12 +3,12 @@ const s = '[현대] 더 뉴아반떼MD 1.6 GDi 프리미엄';
 console.log(s); // [현대] 더 뉴아반떼MD 1.6 GDi 프리미엄
 
 // matchAll을 사용하여 정규표현식으로 매칭된 결과를 얻습니다.
-const matchAllResults = s.matchAll(/\[(.+?)\]/g);
-console.log(matchAllResults);
+const makerNames = s.matchAll(/\[(.+?)\]/g);
+console.log(makerNames);
 
-for (const match of matchAllResults) {
-    console.log(match[0]); // 대괄호 포함
-    console.log(match[1]);
+for (const makerName of makerNames) {
+    console.log(makerName[0]); // 대괄호 포함
+    console.log(makerName[1]);
 }
 
 /*
@@ -27,13 +27,20 @@ console.log(s); //[현대] 더 뉴아반떼MD 1.6 GDi 프리미엄
 var makerNames = s.match(/\[(.+?)\]/g);
 console.log(makerNames);
 console.log(makerNames[0]);
+for (const makerName of makerNames) {    
+    console.log(makerName);
+};
 makerNames.forEach(makerName => {
     console.log(makerName);
 });
-
+    
+    
 makerNames = makerNames.map(m => m.replace(/[\[\]]/g, ''));
 console.log(makerNames);
 console.log(makerNames[0]);
+for (const makerName of makerNames) {    
+    console.log(makerName);
+};
 makerNames.forEach(makerName => {
     console.log(makerName);
 });
@@ -43,9 +50,9 @@ makerNames.forEach(makerName => {
 [ '[현대]' ]
 [현대]
 [현대]
+[현대]
 [ '현대' ]
 현대
 현대
+현대
 */
-
----
