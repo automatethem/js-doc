806c0d5ffe1ab1f07795f727d229f2641f32b8e4
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2024-07-14 22:48:04 +0900

    Create p.js

---
+++ promise/p.js
@@ -0,0 +1,18 @@
+import Results from '@/components/Results';
+const API_KEY process.env.API_KEY;
+export default async function Home({searchParams })
+const genre searchParams.genre 'fetchTrending';
+const res await new Promise((resolve)=>{
+setTimeout(async ()=>
+const response await fetch(
+https://api.themoviedb.org/3s{
+genre ==='fetchTopRated'?/movie/top_rated:/trending/all/week'
+}?api_key=${API_KEY&Language=en-US&page=1',
+next:revalidate:10 }
+)；
+resolve(response);
+2000);//Delay of 2 seconds
+}):
+const data await res.json();
+if (!res.ok){
+throw new Error('Failed to fetch data');
